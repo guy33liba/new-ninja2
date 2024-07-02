64f028bbd07825d02d71c6c8846adbc60e9859da
Guy Liba <guy33liba@gmail.com>
2024-07-02 16:39:31 +0200

    fasd

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
-import router from "./routes/workouts"
+import router from "./routes/workouts.js"
+import mongoose from "mongoose"
+
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
+
 app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -13,7 +16,13 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("welcome")
 })
+
 app.use("/api/workouts", router)
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
-})
+mongoose
+  .connect(process.env.mongoUri)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`)
+    })
+  })
+  .catch((err) => console.log(err))
