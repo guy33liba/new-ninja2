02f81c06339c2bf92cbd1874c9e41df02167f2bf
Guy Liba <guy33liba@gmail.com>
2024-07-03 17:26:01 +0200

    21

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react"
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts")
+      const json = await response.json()
+      if (response.ok) {
+        setWorkouts(json)
+      }
+      fetchWorkout()
+    }
+  }, [])
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => {
+            return  <p key={workouts._id}>{workout.title}</p>
+          })}
+      </div>
+    </div>
   )
 }
 
