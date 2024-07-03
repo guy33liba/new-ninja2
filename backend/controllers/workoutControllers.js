93a6ec92fe7539203bd9c83aa1be3be9c49b1a93
Guy Liba <guy33liba@gmail.com>
2024-07-03 17:06:21 +0200

    fsa

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -1,5 +1,5 @@
 import mongoose, { mongo } from "mongoose"
-import Workout from "../models/Workout"
+import Workout from "../models/Workout.js"
 
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body
