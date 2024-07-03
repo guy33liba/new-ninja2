f354de2273d154a495ee8d02fdb7267e5c653e83
Guy Liba <guy33liba@gmail.com>
2024-07-03 14:22:00 +0200

    fdas

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-import mongoose, { mongo } from "mongoose"
-
-const Schema = mongoose.Schema
-const workoutSchema = new Schema(
-=======
 import mongoose from "mongoose"
 
+const Schema = mongoose.Schema
+
 const workoutSchema = mongoose.Schema(
->>>>>>> 63a3051aabfc5d931307e41c2fe6ff72c370af14
   {
     title: { type: String, required: true },
     reps: { type: String, required: true },
     load: { type: String, required: true },
   },
-<<<<<<< HEAD
   { timestamps: true },
 )
-=======
   { timestamps: true }
 )
 
->>>>>>> 63a3051aabfc5d931307e41c2fe6ff72c370af14
 const Workout = mongoose.model("Workout", workoutSchema)
 export default Workout
